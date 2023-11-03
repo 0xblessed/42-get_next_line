808718b785613c927d390a89b3585a64897b87f6
0xblessed <alemube2002@gmail.com>
2023-11-03 18:06:33 +0100

    Alfa GNL

---
+++ get_next_line_funciona.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 10
+#endif
+
+size_t	ft_strlen(const char *c)
+{
+	size_t	i;
+
+	i = 0;
+	while (c[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(const char *src)
+{
+	char	*srt;
+	int		i;
+	int		j;
+
+	j = ft_strlen(src);
+	srt = (char *) malloc(sizeof(char) * (j + 1));
+	if (srt == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (src[i] != '\0')
+	{
+		srt[i] = src[i];
+		i++;
+	}
+	srt[i] = '\0';
+	return (srt);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	c = (char)c;
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return ((char *)s);
+		}
+		s++;
+	}
+	if (*s == c)
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	size_t	len;
+	size_t	i;
+
+	if (!s1 || !s2)
+		return (NULL);
+	dest = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	len = 0;
+	while (s1 && i < ft_strlen(s1))
+		dest[len++] = s1[i++];
+	i = 0;
+	while (s2 && i < ft_strlen(s2))
+		dest[len++] = s2[i++];
+	dest[len] = '\0';
+	return (dest);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+	size_t	len_s;
+
+	len_s = ft_strlen(s);
+	if (!s)
+		return (NULL);
+	if (start > len_s)
+		return (ft_strdup(""));
+	len_s -= start;
+	if (len_s < len)
+		len = len_s;
+	substr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
+
+char *get_next_line(int fd)
+{
+    ssize_t bytes; 
+    char *buffer;
+    char *linea = "";
+    char *support;
+    static char* sequeda = "";
+    
+    linea = ft_strjoin(linea, sequeda);
+    buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    if (!buffer)
+        return (NULL);
+    bytes = read(fd, buffer, BUFFER_SIZE);
+    while (bytes > 0)
+    {
+        
+        if (support = ft_strchr(buffer, '\n'))
+        {
+            linea = ft_strjoin(linea,ft_substr(buffer, 0, (-1*(ft_strlen(support) - ft_strlen(buffer) - 1))));
+            sequeda = ft_substr(buffer, (-1*(ft_strlen(support) - ft_strlen(buffer) - 1)), BUFFER_SIZE);
+            break;
+        } // encuentra \n, copiar en la frase a devolver hasta el caracter, y copiar la otra parte en el char statico
+        //printf("hola");
+        linea = ft_strjoin(linea, buffer);
+        bytes = read(fd, buffer, BUFFER_SIZE);
+    }
+    printf("%s", linea);
+    //printf("%s", sequeda);
+}
+/*char *get_next_line(int fd)
+{
+    ssize_t bytes; 
+    char *buffer;
+    
+    buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    if (!buffer)
+        return (NULL);
+    bytes = read(fd, buffer, BUFFER_SIZE);
+    while (bytes > 0)
+    {
+        
+        if (ft_strchr(buffer, '\n'))
+            break;
+        printf("hola");
+        bytes = read(fd, buffer, BUFFER_SIZE);
+    }
+}*/
+
+int main()
+{
+
+    int fd;
+
+    fd = open("pepe.txt", O_RDONLY);
+
+    if (fd == -1)
+    {
+        perror("Error al abrir el archivo");
+        return 1;
+    }
+    /*char *frase = */get_next_line(fd);
+    get_next_line(fd);
+    //get_next_line(fd);
+    //printf("%s", frase);
+    close(fd);
+
+    return 0;
+}
